acd89363829c0ae406d4b0bff72b7367ec7de6b0
Josh Meredith <joshmeredith2008@gmail.com>
2018-06-21 19:38:34 +1000

    implement SEIR model

---
+++ outbreak/src/SEIR.scala
@@ -0,0 +1,45 @@
+object SEIR {
+  def apply(
+    nodes: IndexedSeq[NodeData],
+    paths: IndexedSeq[Path],
+    exposureRate:  Float,
+    infectionRate: Float,
+    recoveryRate:  Float,
+    timeSteps:     Int
+  ): Stream[World] = {
+    Stream.iterate(World(nodes, paths), timeSteps + 1) { world: World =>
+      for (here <- world) yield {
+        val Populations(s, e, i, r) = here.populations
+
+        val newlyExposed   = exposureRate * i * s / here.populations.total
+        val newlyInfected  = infectionRate * e
+        val newlyRecovered = recoveryRate * i
+
+        val Populations(departureS, departureE, departureI, departureR) =
+          (for ((path, _) <- here.outgoing) yield {
+            val f = path.flow
+            val n = here.populations.total
+            Populations(f * s / n, f * e / n, f * i / n, f * r / n)
+          }) . foldLeft(Populations.zero)(_+_)
+
+        val Populations(arrivalS, arrivalE, arrivalI, arrivalR) =
+          (for ((path, origin) <- here.incoming) yield {
+            val f = path.flow
+            val n = origin.populations.total
+            val Populations(sO, eO, iO, rO) = origin.populations
+            val infected = f * iO / n
+            val quarantined = infected * path.quarantineRate
+            val notQuarantined = infected - quarantined
+            Populations(f * sO / n, f * eO / n, notQuarantined, f * rO / n + quarantined)
+          }) . foldLeft(Populations.zero)(_+_)
+
+        val deltaS = -newlyExposed                                   - departureS + arrivalS
+        val deltaE =  newlyExposed - newlyInfected                   - departureE + arrivalE
+        val deltaI =                 newlyInfected  - newlyRecovered - departureI + arrivalI
+        val deltaR =                                  newlyRecovered - departureR + arrivalR
+
+        Populations(s + deltaS, e + deltaE, i + deltaI, r + deltaR)
+      }
+    }
+  }
+}
