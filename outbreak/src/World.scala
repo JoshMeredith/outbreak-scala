5f266d6e55e3a61e2c3f93ef80ef1db78d093c5d
Josh Meredith <joshmeredith2008@gmail.com>
2019-04-03 15:42:25 +1100

    Store paths in World using vectors instead of maps

--- outbreak/src/World.scala
+++ outbreak/src/World.scala
@@ -1,23 +1,42 @@
-object World {
-  def apply[n](nodes: IndexedSeq[n], paths: IndexedSeq[Path]): World[n] = {
-    val arrivals   = paths.groupBy(_.destination).mapValues(Paths(_))
-    val departures = paths.groupBy(_.origin     ).mapValues(Paths(_))
+import scala.collection.parallel.mutable._
+import java.time._
 
-    new World[n](nodes, arrivals, departures)
+object World {
+  def apply[n](nodes: IndexedSeq[n], paths: IndexedSeq[(Int, Path)]): World[n] = {
+
+    val arrivals = paths.groupBy(x => (x._1, x._2.destination)).mapValues { case ps =>
+      Paths(ps.map(_._2))
+    }.withDefault(x => Paths(Seq()))
+
+    val departures = paths.groupBy(x => (x._1, x._2.origin)).mapValues { case ps =>
+      Paths(ps.map(_._2))
+    }.withDefault(x => Paths(Seq()))
+
+    val arrivalsV   = (0 to 11).toVector.map( m =>
+      (0 until nodes.length).toVector.map( i => arrivals  (m + 1, i) ))
+
+    val departuresV = (0 to 11).toVector.map( m =>
+      (0 until nodes.length).toVector.map( i => departures(m + 1, i) ))
+
+    new World[n](nodes, arrivalsV, departuresV, LocalDate.of(2009, 6, 1))
   }
 }
 
 case class World[n](
   nodes:      IndexedSeq[n],
-  arrivals:   Map[Int, Paths],
-  departures: Map[Int, Paths]
+  arrivals:   Vector[Vector[Paths]],
+  departures: Vector[Vector[Paths]],
+  date:       LocalDate
 ) {
-  def apply(i: Int): Node[n] = Node[n](nodes(i), arrivals(i), departures(i), this)
+  private val m = date.getMonthValue - 1
+
+  def apply(i: Int): Node[n] =
+    Node[n](nodes(i), arrivals(m)(i), departures(m)(i), this)
 
   def step(f: Node[n] => n): World[n] = {
-    val newNodes = nodes.zipWithIndex.map { case (n, i) =>
-      f(Node(n, arrivals(i), departures(i), this))
+    var newNodes = nodes.zipWithIndex.par.map { case (n, i) =>
+      f(Node(n, arrivals(m)(i), departures(m)(i), this))
     }
-    this.copy(nodes = newNodes)
+    this.copy(nodes = newNodes.toVector, date = date.plusDays(1))
   }
 }
